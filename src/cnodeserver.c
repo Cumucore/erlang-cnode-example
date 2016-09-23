fbedc1fd10cc3a7dd2620bb4ab6ec1ade3730789
Vicent Ferrer <vicent.ferrerguasch@aalto.fi>
2016-09-23 18:39:43 +0300

    Use of ei instead of erl_interface. Bidirectional requests

--- src/cnodeserver.c
+++ src/cnodeserver.c
@@ -34,51 +34,92 @@ typedef struct{
 typedef struct{
   struct event_base *evbase;
   struct event      *listen_ev;
+  ei_cnode          ec;
   ConnT             connList[30];
 }ServerT;
 
+void handle_msg(int fd, erlang_msg *emsg, ei_x_buff *buf1){
+  int i = 0, j= 0, arity = 0;
+  unsigned long arg = 0;
+  unsigned char funAtom[MAXATOMLEN+1] = {0};
+  int res;
+  char *s;
+
+  ei_decode_version(buf1->buff, &i, NULL);
+
+  j = i;
+  s = malloc(BUFSIZE);
+  bzero(s, BUFSIZE);
+  if(ei_s_print_term(&s, buf1->buff, &j)==-1){
+    fprintf(stderr,"Error 1\n");
+    return;
+  }
+  printf("Received term : %s\n", s);
+  free(s);
+
+  if (emsg->msgtype == ERL_REG_SEND) {
+    ei_decode_tuple_header(buf1->buff, &i, &arity);
+    if(arity != 3){
+      fprintf(stderr, "Wrong message format 1, arity =%d\n", arity);
+      return;
+    }
+    ei_skip_term(buf1->buff, &i); /* call atom*/
+    ei_skip_term(buf1->buff, &i); /* from*/
+    ei_decode_tuple_header(buf1->buff, &i, &arity);
+    if(arity != 2){
+      fprintf(stderr, "Wrong message format 2\n");
+      return;
+    }
+    ei_decode_atom(buf1->buff, &i, funAtom);
+    ei_decode_ulong(buf1->buff, &i, &arg);
+
+    if (strncmp(funAtom, "foo", 3) == 0) {
+      res = foo(arg);
+    } else if (strncmp(funAtom, "bar", 3) == 0) {
+      res = bar(arg);
+    }
+
+    /*Send Response*/
+    ei_x_buff rsp;
+    ei_x_new(&rsp);
+    ei_x_format(&rsp, "{cnode, ~i}", res);
+    if(ei_send(fd, &emsg->from, rsp.buff, rsp.index)<0){
+      fprintf(stderr,"ei_send\n");
+    }
+    ei_x_free(&rsp);
+  }
+}
+
 void process_msg(evutil_socket_t fd, short event, void *arg){
   int got;                                 /* Result of receive */
-  unsigned char buf[BUFSIZE];              /* Buffer for incoming message */
-  ErlMessage emsg;                         /* Incoming message */
   ServerT *self = (ServerT *)arg;
 
-  ETERM *fromp, *tuplep, *fnp, *argp, *resp;
-  int res;
+  ei_x_buff buf1;
+  erlang_msg emsg = {0};                         /* Incoming message */
 
-  got = erl_receive_msg(fd, buf, BUFSIZE, &emsg);
+  ei_x_new(&buf1);
+  got = ei_xreceive_msg(fd, &emsg, &buf1);
   if (got == ERL_TICK) {
     /* ignore */
+      /*Send Req*/
+      ei_x_buff req;
+      ei_x_new(&req);
+      ei_x_format(&req, "{cnode, ~p, {efoo, ~i}}", ei_self(&self->ec), 10);
+      if(ei_reg_send(&self->ec, fd, "test", req.buff, req.index)<0){
+          fprintf(stderr,"ei_send\n");
+      }
+      ei_x_free(&req);
   } else if (got == ERL_ERROR) {
     fprintf(stderr, "Error received from node %s. Closing\n",
             self->connList[fd].conn.nodename);
     event_del(self->connList[fd].ev);
     close(fd);
     return;
-  } else {
+  } else if (got == ERL_MSG){
     printf("Message from node %s\n", self->connList[fd].conn.nodename);
-
-    if (emsg.type == ERL_REG_SEND) {
-      fromp = erl_element(2, emsg.msg);
-      tuplep = erl_element(3, emsg.msg);
-      fnp = erl_element(1, tuplep);
-      argp = erl_element(2, tuplep);
-
-      if (strncmp(ERL_ATOM_PTR(fnp), "foo", 3) == 0) {
-        res = foo(ERL_INT_VALUE(argp));
-      } else if (strncmp(ERL_ATOM_PTR(fnp), "bar", 3) == 0) {
-        res = bar(ERL_INT_VALUE(argp));
-      }
-
-      resp = erl_format("{cnode, ~i}", res);
-      erl_send(fd, fromp, resp);
-
-      erl_free_term(emsg.from); erl_free_term(emsg.msg);
-      erl_free_term(fromp); erl_free_term(tuplep);
-      erl_free_term(fnp); erl_free_term(argp);
-      erl_free_term(resp);
-    }
+    handle_msg(fd, &emsg, &buf1);
   }
+  ei_x_free(&buf1);
 }
 
 void accept_new_node(evutil_socket_t ss, short event, void *arg){
@@ -86,7 +127,7 @@ void accept_new_node(evutil_socket_t ss, short event, void *arg){
   ErlConnect conn;                         /* Connection data */
   ServerT *self = (ServerT *)arg;
 
-  if ((fd = erl_accept(ss, &conn)) == ERL_ERROR)
+  if ((fd = ei_accept(&self->ec, ss, &conn)) == ERL_ERROR)
       erl_err_quit("erl_accept");
   fprintf(stderr, "Connected to %s\n\r", conn.nodename);
   self->connList[fd].fd = fd;
@@ -129,19 +170,19 @@ int main(int argc, char **argv) {
 
   port = atoi(argv[1]);
 
-  /*Initialize Erl_Interface*/
-  erl_init(NULL, 0);
+  /* /\*Initialize Erl_Interface*\/ */
+  /* erl_init(NULL, 0); */
 
   /*Initialize C-node*/
-  if (erl_connect_init(1, "secretcookie", 0) == -1)
-    erl_err_quit("erl_connect_init");
+  if (ei_connect_init(&self.ec, "c1", "secretcookie", 0) == -1)
+    erl_err_quit("ei_connect_init");
 
   /* Make a listen socket */
   if ((listen = my_listen(port)) <= 0)
     erl_err_quit("my_listen");
 
-  if (erl_publish(port) == -1)
-    erl_err_quit("erl_publish");
+  if (ei_publish(&self.ec, port) == -1)
+    erl_err_quit("ei_publish");
 
   self.listen_ev = event_new(self.evbase, listen, EV_READ|EV_PERSIST,
                               (event_callback_fn) accept_new_node, &self);
--- src/complex3.erl
+++ src/complex3.erl
@@ -1,5 +1,5 @@
 -module(complex3).
--export([foo/1, bar/1]).
+-export([foo/1, bar/1, test/0, srv/0]).
 
 foo(X) ->
     call_cnode({foo, X}).
@@ -14,3 +14,25 @@ call_cnode(Msg) ->
     {cnode, Result} ->
         Result
     end.
+
+%% C node invoking erlang functions
+
+efoo(Num) ->
+    io:format("Called efoo~p~n", [Num]),
+    Num + 2.
+
+srv() ->
+    receive
+        {cnode, From , {efoo, Arg}} ->
+            From ! {ok, efoo(Arg)};
+        {cnode, From, Result} ->
+            io:format("~p~n", [Result]),
+            From ! ok
+    end,
+    srv().
+
+test() ->
+    Pid = spawn(?MODULE, srv, []),
+    link(Pid),
+    register(test, Pid),
+    ok.
